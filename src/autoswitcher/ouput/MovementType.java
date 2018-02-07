26bdf3680cf9c145c1ef2ce1287e4e50a8f3d01b
No <nomoonen@gmail.com>
2018-02-07 22:20:48 +0100

    added movementtype enum for mouse movement

---
+++ src/autoswitcher/ouput/MovementType.java
@@ -0,0 +1,18 @@
+package autoswitcher.ouput;
+
+
+/**
+ * the desired movementtypes for moving the mouse choices are:<br>
+ * -straight path
+ * -circle path
+ * -sinus path
+ * -random of the above
+ * 
+ * @author No
+ *
+ */
+public enum MovementType {
+	
+	straight,circle,sinus,random
+
+}
--- src/autoswitcher/ouput/OutputManager.java
+++ src/autoswitcher/ouput/OutputManager.java
@@ -5,17 +5,29 @@ import java.awt.MouseInfo;
 import java.awt.Point;
 import java.awt.Robot;
 
-public class outputManager extends Robot {
 
-	private static outputManager instance;
+/**
+ * This can do all the output for this program
+ * made to be as humanlike as possible
+ * @author No
+ *
+ */
+public class OutputManager extends Robot {
 
-	private outputManager() throws AWTException {
+	private static OutputManager instance;
+
+	private OutputManager() throws AWTException {
 		super();
 	}
 
-	public static outputManager getInstance() throws AWTException {
+	/**
+	 * gets the instance of the outputManager
+	 * @return {@link outputManager}
+	 * @throws AWTException
+	 */
+	public static OutputManager getInstance() throws AWTException {
 		if (instance == null) {
-			instance = new outputManager();
+			instance = new OutputManager();
 		}
 		return instance;
 	}
@@ -28,8 +40,10 @@ public class outputManager extends Robot {
 	 *            - the point to move the mouse to
 	 * @param timeToMoveInMs
 	 *            - The time the movement of the mouse should take
+	 * @param movementType
+	 *            -{@link MovementType} for the desired movementype
 	 */
-	public void moveMouseNormal(Point movePoint, int timeToMoveInMs) {
+	public void moveMouseNormal(Point movePoint, int timeToMoveInMs, MovementType movementType) {
 		Point oldMousePosition = MouseInfo.getPointerInfo().getLocation();
 		long startSystemTime = System.currentTimeMillis();
 		// calculates the distance between the points
@@ -39,11 +53,11 @@ public class outputManager extends Robot {
 		for (int i = 0; i < distance; i++) {
 			mouseMove((int) (oldMousePosition.x + i * xdiff), (int) (oldMousePosition.y + i * ydiff));
 			long timeElapsed = System.currentTimeMillis() - startSystemTime;
-			//waits till the movement is on track with the timing again
-			while(timeElapsed < timeToMoveInMs * (i / distance)){
+			// waits till the movement is on track with the timing again
+			while (timeElapsed < timeToMoveInMs * (i / distance)) {
 				timeElapsed = System.currentTimeMillis() - startSystemTime;
 			}
-			
+
 		}
 		// this last mousemove is to make sure it goes to the right place
 		mouseMove(movePoint.x, movePoint.y);
