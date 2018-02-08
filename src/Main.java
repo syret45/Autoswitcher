ba57b8a2399a8f82a4f48f190f74126f0040b206
No <nomoonen@gmail.com>
2018-02-08 15:07:26 +0100

    changed project to maven project

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import java.awt.AWTException;
 import java.io.IOException;
 
+import autoswitcher.ui.SettingsScreen;
+
 public class Main {
 
 	public static void main(String[] args) throws AWTException, IOException {
 		// TODO Auto-generated method stub
-
+		new SettingsScreen();
 	}
 }
