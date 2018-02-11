3850e4d9863a6c2e0c4b6c6969a8dd8c4f145911
No <nomoonen@gmail.com>
2018-02-11 20:12:04 +0100

    added items

--- src/autoswitcher/switcher/SwitchItem.java
+++ src/autoswitcher/switcher/SwitchItem.java
@@ -58,8 +58,19 @@ public class SwitchItem {
 	//items
  	public static SwitchItem Empty = new SwitchItem("", "");
  	public static SwitchItem Black_Dhide_Body = new SwitchItem("black d'hide body", "Black_d'hide_body.png");
+ 	public static SwitchItem Black_Dhide_chaps = new SwitchItem("black dhide chaps", "Black_dhide_chaps.png");
+ 	public static SwitchItem Enchanted_robe_top = new SwitchItem("Enchanted robe top", "Enchanted_robe_top.png");
+ 	public static SwitchItem Enchanted_robe_bottom = new SwitchItem("Enchanted robe bottom", "Enchanted_robe_bottom.png");
  	public static SwitchItem Rune_Platelegs = new SwitchItem("rune platelegs", "Rune_platelegs.png");
  	public static SwitchItem Avas_Accumulator = new SwitchItem("Avas accumulator", "Avas_Accumulator.png");
+ 	public static SwitchItem Rune_Crossbow = new SwitchItem("Rune Crossbow", "Rune_Crossbow.png");
+ 	public static SwitchItem Dragon_boots = new SwitchItem("Dragon boots", "Dragon_boots.png");
+ 	public static SwitchItem Dragon_defender = new SwitchItem("Dragon defender", "Dragon_defender.png");
+ 	public static SwitchItem Tentacle_whip = new SwitchItem("Tentacle whip", "Tent_whip.png");
+ 	public static SwitchItem Torag_platebody = new SwitchItem("Torag platebody", "Torag_platebody.png");
+ 	public static SwitchItem Torag_platelegs = new SwitchItem("Torag platelegs", "Torag_platelegs.png");
+ 	public static SwitchItem Armadyl_godsword = new SwitchItem("Armadyl godsword", "Armadyl_godsword.png");
+ 	public static SwitchItem Infernal_cape = new SwitchItem("Infernal cape", "Infernal_cape.png");
 
 
 }
