22b40ee08835675b6864967c3992038308318507
No <nomoonen@gmail.com>
2018-04-15 15:00:12 +0200

    made start for itemselectscreen

---
+++ src/autoswitcher/ui/ItemSelectScreen.java
@@ -0,0 +1,13 @@
+package autoswitcher.ui;
+
+import javax.swing.JFrame;
+
+public class ItemSelectScreen extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ItemSelectScreen() {
+		//makes a itemselectscreen
+	}
+
+}
